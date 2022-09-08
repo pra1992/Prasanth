5d3cf1abfc86a98072d48e39d12f21bfb97986a2
pra1992 <58290550+pra1992@users.noreply.github.com>
2022-09-09 00:08:34 +0530

    Add files via upload

---
+++ FindIntersection.java
@@ -0,0 +1,23 @@
+package week1.assignments;
+
+public class FindIntersection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] a1= {3,2,11,4,6,7};
+		int [] a2= {1,2,8,4,9,7};
+	
+		for(int i=0; i<a1.length;i++) {
+			for (int j=0; j<a2.length;j++) {
+				if(a1[i]==a2[j]) {
+					//System.out.println(a2[j]);
+					System.out.println(a1[i]);
+				}
+			}
+		}
+		
+
+	}
+
+}
